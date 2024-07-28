ba2e0e74e6d309b05b0a2210ec529bfabaf959f5
F347-FB <holdok449@gmail.com>
2024-07-28 22:57:15 +0300

    Update Car-Dealership-Tycoon.lua

--- Car-Dealership-Tycoon.lua
+++ Car-Dealership-Tycoon.lua
@@ -1,2 +1,2 @@
-setclipboard("loadstring(game:HttpGet("https://raw.githubusercontent.com/F347-FB/Roblox/main/Loader"))()")
 local yes = game.Players.LocalPlayer  yes:kick("Telegram channel where the new loder is located copied to the clipboard")
+setclipboard("https://t.me/moonteam10")
